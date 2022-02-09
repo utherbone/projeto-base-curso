9f28167cc39914ea457548ab4573528c560a1f64
utherbone <gabriel.saraiva.dev@outlook.com>
2022-02-09 02:03:10 -0400

    Configurando um template estatico

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,19 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('home');
+})->name('home');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+Route::get('/post', function () {
+    return view('post');
+})->name('post');
+
+Route::get('/contact', function () {
+    return view('contact');
+})->name('contact');
+
+Route::get('/about', function () {
+    return view('about');
+})->name('about');
 
 require __DIR__.'/auth.php';
